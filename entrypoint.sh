bf47d324a9ab67dba5dc28bf594caefe1be06261
Martin Beentjes <martin@beentjes.me>
2020-02-07 22:41:00 +0100

    Alter version bump to implement prerelease

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,47 @@
 #     pipeline exit successfully (-o pipefail)
 set -euo pipefail
 
-main() {
+# This scripts bumps version with major, minor or patch. If set to true the version will be postfixed with an postfix
+# 1.0.0-alpha-1
 
-  prev_version="$1"; release_type="$2"
+
+
+# major.minor.patch-postfix-preversion
+
+configure_postfix() {
+  if [[ ! -z "$preversion" ]]; then
+    ((++preversion))
+  else 
+    preversion=1
+  fi 
+  pre="-alpha-$preversion"
+}
+
+bump_version() {
+  case "$release_type" in
+    "major")
+      ((++major)); minor=0; patch=0; pre="";;
+    "minor")
+      ((++minor)); patch=0; pre="";;
+    "patch")
+      ((++patch)); pre="";;
+    "prerelease")
+
+  esac
+}
+
+main() {
+  if [[ "$#" -ne 2 ]]; then
+    echo "Must have two arguments: previous_version release_type"; exit 1
+  fi
+
+  prev_version="$1"; release_type="$2";
 
   if [[ "$prev_version" == "" ]]; then
     echo "could not read previous version"; exit 1
   fi
 
-  possible_release_types="major feature bug alpha beta rc"
+  possible_release_types="major minor patch prerelease"
 
   if [[ ! ${possible_release_types[*]} =~ ${release_type} ]]; then
     echo "valid argument: [ ${possible_release_types[*]} ]"; exit 1
@@ -26,7 +58,7 @@ main() {
   major=0; minor=0; patch=0; pre=""; preversion=0
 
   # break down the version number into it's components
-  regex="^([0-9]+).([0-9]+).([0-9]+)((-[a-z]+)([0-9]+))?$"
+  regex="^([0-9]+).([0-9]+).([0-9]+)((-[a-z]+-)([0-9]+))?$"
   if [[ $prev_version =~ $regex ]]; then
     major="${BASH_REMATCH[1]}"
     minor="${BASH_REMATCH[2]}"
@@ -38,47 +70,16 @@ main() {
     exit 1
   fi
 
-  # increment version number based on given release type
-  case "$release_type" in
-  "major")
-    ((++major)); minor=0; patch=0; pre="";;
-  "feature")
-    ((++minor)); patch=0; pre="";;
-  "bug")
-    ((++patch)); pre="";;
-  "alpha")
-    if [[ ! -z "$preversion" ]]; then
-      preversion=0
-    fi
-    ((++preversion))
-    if [[ "$pre" != "-alpha" ]]; then
-      preversion=1
-    fi
-    pre="-alpha$preversion";;
-  "beta")
-    if [[ ! -z "$preversion" ]]; then
-      preversion=0
-    fi
-    ((++preversion))
-    if [[ "$pre" != "-beta" ]]; then
-      preversion=1
-    fi
-    pre="-beta$preversion";;
-  "rc")
-    if [[ ! -z "$preversion" ]]; then
-      preversion=0
-    fi
-    ((++preversion))
-    if [[ "$pre" != "-rc" ]]; then
-      preversion=1
-    fi
-    pre="-rc$preversion";;
-  esac
-
+  if [[ "$release_type" == "prerelease" ]]; then
+    configure_postfix 
+  else
+    bump_version
+  fi
+  
   next_version="${major}.${minor}.${patch}${pre}"
   echo "create $release_type-release version: $prev_version -> $next_version"
 
   echo ::set-output name=next-version::"$next_version"
 }
 
-main "$1" "$2"
+main "$@"
